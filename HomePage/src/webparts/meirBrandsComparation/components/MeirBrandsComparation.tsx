ced5b90dc411fb515dcba54869745ed786bfee90
Chen Hagag <chen.hagag@gmail.com>
2024-08-26 13:35:29 +0300

    Updates

--- HomePage/src/webparts/meirBrandsComparation/components/MeirBrandsComparation.tsx
+++ HomePage/src/webparts/meirBrandsComparation/components/MeirBrandsComparation.tsx
@@ -1,19 +1,74 @@
 import * as React from 'react';
 import styles from './MeirBrandsComparation.module.scss';
 import { IMeirBrandsComparationProps } from './IMeirBrandsComparationProps';
+import { Utils } from '../../../Services/Utils';
+import { sp } from "@pnp/sp";
+import '@pnp/sp/site-users/web';
 
 const siteURL = "KBMCT2"; //KBMCT1
 
-export default class MeirBrandsComparation extends React.Component<IMeirBrandsComparationProps, {}> {
-  public render(): React.ReactElement<IMeirBrandsComparationProps> {
-    const links = [
-      { text: 'הונדה רכבים- Volvo Selekt', url: 'https://example.com/1' },
-      { text: 'Volvo אוטובוסים- Zhong Tong אוטובוסים', url: 'https://example.com/2' },
-      { text: 'וולוו משאיות - רנו משאיות', url: 'https://example.com/3' },
-      { text: 'פולסטאר - NIU', url: 'https://example.com/4' },
-      { text: 'פולסטאר - וולוו רכבים', url: 'https://example.com/5' },
-    ];
+export interface IItem {
+  Title: string;
+  Code: string;
+}
 
+export interface ICompareModelsState { 
+  allItems: IItem[];
+}
+
+export default class MeirBrandsComparation extends React.Component<IMeirBrandsComparationProps, ICompareModelsState> {
+  
+  private _utils: Utils;
+
+  constructor(props: IMeirBrandsComparationProps) {
+    super(props);
+    this._utils = new Utils();
+    this.state = {
+      allItems: []
+    };
+  }
+
+  private GetItems = async () => {
+    try {
+      const currentUser = await sp.web.currentUser.get();
+
+      const items: IItem[] = await this._utils.GetItems(
+        "השוואה בין דגמים",
+        "Title,Code",
+        null,
+        "Created",
+        false, 
+        `(Author/Id eq ${currentUser.Id} or Editor/Id eq ${currentUser.Id})`, 
+        6
+      );
+
+      console.log("Brand Compare Items: ", items);
+      this.setState({ allItems: items });
+    } catch (error) {
+      console.error('Error fetching items', error);
+    }
+  }
+
+  public componentDidMount() {
+    this.GetItems();
+  }
+
+  public render(): React.ReactElement<IMeirBrandsComparationProps> {
+   
+    const { allItems } = this.state;
+    const noItemsMessage = (
+      <div>
+        עוד לא יצרת השוואות בין דגמים, להוספה ,{" "}
+        <a
+          href={`/sites/${siteURL}/SitePages/BrandsComparation.aspx`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          לחצ/י כאן
+        </a>.
+      </div>
+    );
+  
     return (
       <section className={styles.meirBrandsComparation}>
         <div className={styles.updateArea}>
@@ -23,17 +78,41 @@ export default class MeirBrandsComparation extends React.Component<IMeirBrandsCo
                 <div className={styles.imageArea}>
                   <div
                     className={styles.theImage}
-                    style={{ backgroundImage: `url('/sites/${siteURL}/SiteAssets/cut/compare.svg')` }}
+                    style={{
+                      backgroundImage: `url('/sites/${siteURL}/SiteAssets/cut/compare.svg')`,
+                    }}
                   ></div>
                 </div>
                 <div className={styles.data}>
-                  <div className={styles.title}>השוואה בין דגמים</div>
+                  <div
+                    className={styles.title}
+                    onClick={() =>
+                      this._utils.OpenTab(
+                        `/sites/${siteURL}/SitePages/BrandsComparation.aspx`
+                      )
+                    }
+                  >
+                    השוואה בין דגמים
+                  </div>
                   <div className={styles.text}>
-                    {links.map((link, index) => (
-                      <a key={index} href={link.url} target="_blank" rel="noopener noreferrer" className={styles.link}>
-                        {link.text}
-                      </a>
-                    ))}
+                    {allItems.length === 0 ? (
+                      noItemsMessage
+                    ) : (
+                      allItems.map((item, index) => (
+                        <a
+                          key={index}
+                          href={`/sites/${siteURL}/SitePages/BrandsComparation.aspx?BrandsCode=${item.Code}`}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className={styles.tooltipContainer}
+                        >
+                          {this._utils.limitChars(item.Title, 30)}
+                          <span className={styles.tooltipText}>
+                            {item.Title}
+                          </span>
+                        </a>
+                      ))
+                    )}
                   </div>
                 </div>
               </div>
@@ -43,4 +122,5 @@ export default class MeirBrandsComparation extends React.Component<IMeirBrandsCo
       </section>
     );
   }
+  
 }
--- HomePage/src/webparts/meirUpdates/components/MeirUpdates.tsx
+++ HomePage/src/webparts/meirUpdates/components/MeirUpdates.tsx
@@ -1,46 +1,74 @@
 import * as React from 'react';
 import styles from './MeirUpdates.module.scss';
 import { IMeirUpdatesProps } from './IMeirUpdatesProps';
+import { Utils } from '../../../Services/Utils';
 
-const siteURL = "KBMCT2"; // KBMCT1
+const siteURL = "KBMCT2";
 
 interface IUpdateItem {
-  id: number;
-  title: string;
-  date: string;
-  text: string;
-  description: string;
-  imageUrl: string;
+  ID: number;
+  UpdateType: any;
+  UpdateDate: string;
+  Title: string;
+  Detail1: string;
+  Picture1: any;
 }
 
 interface IMeirUpdatesState {
+  allItems: IUpdateItem[];
   selectedUpdate: IUpdateItem | null;
   isPopupVisible: boolean;
 }
 
 export default class MeirUpdates extends React.Component<IMeirUpdatesProps, IMeirUpdatesState> {
-  private updates: IUpdateItem[] = [
-    { id: 1, title: 'דגמים חדשים', date: "יום ה' 30 במאי 2024", text: 'NQiGTs-Cargo-5-1', description: 'תיאור דגם חדש', imageUrl: `/sites/${siteURL}/SiteAssets/cut/NQiGTs-Cargo-5-1.png` },
-    { id: 2, title: 'דגם אחר', date: "יום ב' 15 ביוני 2024", text: 'כותרת דגם נוסף', description: 'תיאור דגם נוסף', imageUrl: `/sites/${siteURL}/SiteAssets/cut/sampleCar2.png` },
-    { id: 3, title: 'עוד דגם', date: "יום ג' 10 ביולי 2024", text: 'עוד כותרת דגם', description: 'תיאור דגם אחר', imageUrl: `/sites/${siteURL}/SiteAssets/cut/sampleCar3.png` },
-    { id: 4, title: 'דגמים חדשים', date: "יום ה' 30 במאי 2024", text: 'כותרת דגם חדש', description: 'תיאור דגם חדש נוסף', imageUrl: `/sites/${siteURL}/SiteAssets/cut/sampleCar.png` },
-    { id: 5, title: 'דגם אחר', date: "יום ב' 15 ביוני 2024", text: 'כותרת דגם נוסף', description: 'תיאור דגם נוסף אחר', imageUrl: `/sites/${siteURL}/SiteAssets/cut/sampleCar2.png` }, 
-  ];
+  private _utils: Utils;
 
   constructor(props: IMeirUpdatesProps) {
     super(props);
+    this._utils = new Utils();
     this.state = {
-      selectedUpdate: this.updates[0],
+      allItems: [],
+      selectedUpdate: null,
       isPopupVisible: false,
     };
   }
 
+  private GetItems = async () => {
+    try {
+      const items: IUpdateItem[] = await this._utils.GetItems(
+        "עדכונים",
+        "Title,ID,UpdateType/Title,UpdateDate,Detail1,Picture1",
+        "UpdateType",
+        "Created",
+        false, 
+        null, 
+        5
+      );
+
+      // Parse the Picture1 JSON string into an object
+      const parsedItems = items.map(item => ({
+        ...item,
+        Picture1: item.Picture1 ? JSON.parse(item.Picture1) : null
+      }));
+
+      console.log("Parsed Updates Items: ", parsedItems);
+      this.setState({ 
+        allItems: parsedItems, 
+        selectedUpdate: parsedItems[0] // Select the first update on load
+      });
+    } catch (error) {
+      console.error('Error fetching items', error);
+    }
+  }
+
+  public componentDidMount() {
+    this.GetItems();
+  }
+
   handleUpdateClick = (update: IUpdateItem) => {
-    if (this.state.selectedUpdate?.id === update.id) {
-      // If the update is already selected, toggle the popup
+    if (this.state.selectedUpdate?.ID === update.ID) {
       this.setState({ isPopupVisible: !this.state.isPopupVisible });
     } else {
-      // Select the update and hide the popup
       this.setState({ selectedUpdate: update, isPopupVisible: false });
     }
   };
@@ -50,7 +78,7 @@ export default class MeirUpdates extends React.Component<IMeirUpdatesProps, IMei
   };
 
   render(): React.ReactElement<IMeirUpdatesProps> {
-    const { selectedUpdate, isPopupVisible } = this.state;
+    const { allItems, selectedUpdate, isPopupVisible } = this.state;
 
     return (
       <section className={styles.meirUpdates}>
@@ -62,22 +90,22 @@ export default class MeirUpdates extends React.Component<IMeirUpdatesProps, IMei
             </div>
             <div className={styles.upList}>
               <div className={styles.right}>
-                {this.updates.map((update) => (
+                {allItems.map((update) => (
                   <div
-                    key={update.id}
-                    className={`${styles.oneUpdate} ${selectedUpdate?.id === update.id ? styles.oneUpdateSelected : ''}`}
+                    key={update.ID}
+                    className={`${styles.oneUpdate} ${selectedUpdate?.ID === update.ID ? styles.oneUpdateSelected : ''}`}
                     onClick={() => this.handleUpdateClick(update)}
                   >
                     <div className={styles.icon}>
-                      <img src={`/sites/${siteURL}/SiteAssets/cut/favorite.svg`} alt={`Icon for ${update.title}`} />
+                      <img src={`/sites/${siteURL}/SiteAssets/cut/favorite.svg`} alt={`Icon for ${update.UpdateType?.Title || 'Update'}`} />
                     </div>
                     <div className={styles.data}>
                       <div className={styles.r100}>
-                        <div className={styles.title}>{update.title}</div>
-                        <div className={styles.date}>{update.date}</div>
+                        <div className={styles.title}>{update.UpdateType?.Title || 'Update Type'}</div>
+                        <div className={styles.date}>{this._utils.formatDate(update.UpdateDate)}</div>
                       </div>
                       <div className={styles.text}>
-                        {update.text}
+                        {update.Title}
                       </div>
                     </div>
                   </div>
@@ -86,11 +114,12 @@ export default class MeirUpdates extends React.Component<IMeirUpdatesProps, IMei
               <div className={styles.oneUpdateDetails}>
                 {selectedUpdate && (
                   <>
-                    <div className={styles.r100} onClick={() => this.setState({ isPopupVisible: true })}>
-                      <img src={selectedUpdate.imageUrl} alt={`image for ${selectedUpdate.title}`} />
-                    </div>
-                    <div className={styles.text} onClick={() => this.setState({ isPopupVisible: true })}>
-                      {selectedUpdate.description} 
+                    {selectedUpdate.Picture1 && selectedUpdate.Picture1.serverRelativeUrl && (
+                      <div className={styles.r100} onClick={() => this.setState({ isPopupVisible: true })}>
+                        <img src={selectedUpdate.Picture1.serverRelativeUrl} alt={`image for ${selectedUpdate.Title}`} />
+                      </div>
+                    )}
+                    <div className={styles.text} onClick={() => this.setState({ isPopupVisible: true })} dangerouslySetInnerHTML={{ __html: selectedUpdate.Detail1 }}>
                     </div>
                   </>
                 )}
@@ -102,10 +131,11 @@ export default class MeirUpdates extends React.Component<IMeirUpdatesProps, IMei
         {isPopupVisible && selectedUpdate && (
           <div className={styles.popup}>
             <div className={styles.popupContent}>
-              <h2>{selectedUpdate.title}</h2> 
-              <img src={selectedUpdate.imageUrl} alt={`Popup image for ${selectedUpdate.title}`} />
-              <div className={styles.popupText}>
-                {selectedUpdate.description} 
+              <h2>{selectedUpdate.Title}</h2>
+              {selectedUpdate.Picture1 && selectedUpdate.Picture1.serverRelativeUrl && (
+                <img src={selectedUpdate.Picture1.serverRelativeUrl} alt={`Popup image for ${selectedUpdate.Title}`} />
+              )}
+              <div className={styles.popupText} dangerouslySetInnerHTML={{ __html: selectedUpdate.Detail1 }}>
               </div>
               <button className={styles.closeButton} onClick={this.handlePopupClose}>קראתי, הבנתי</button>
             </div>
